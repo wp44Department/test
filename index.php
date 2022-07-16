366b78a50650a3238e057d829ed1dba4ba32e8a4
wp44Department <wp44.department@gmail.com>
2022-07-16 19:50:25 +0530

    have added a new code in footer,header,page,sidebar,single,style.css files

--- index.php
+++ index.php
@@ -1,45 +1,19 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-
-get_header(); ?>
-
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-	<header class="page-header alignwide">
-		<h1 class="page-title"><?php single_post_title(); ?></h1>
-	</header><!-- .page-header -->
+<?php get_header(); ?>
+<main class="wrap">
+  <section class="content-area content-thin">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <article class="article-loop">
+        <header>
+          <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+          By: <?php the_author(); ?>
+        </header>
+        <?php the_excerpt(); ?>
+      </article>
+<?php endwhile; else : ?>
+      <article>
+        <p>Sorry, no posts were found!</p>
+      </article>
 <?php endif; ?>
-
-<?php
-if ( have_posts() ) {
-
-	// Load posts loop.
-	while ( have_posts() ) {
-		the_post();
-
-		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
-	}
-
-	// Previous/next page navigation.
-	twenty_twenty_one_the_posts_navigation();
-
-} else {
-
-	// If no content, include the "No posts found" template.
-	get_template_part( 'template-parts/content/content-none' );
-
-}
-
-get_footer();
+  </section><?php get_sidebar(); ?>
+</main>
+<?php get_footer(); ?>
